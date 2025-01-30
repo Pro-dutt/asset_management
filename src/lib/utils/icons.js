e42f1ce8b8da18e22750d9c75490b1d2434b6a94
Aausafalam <aausaf@c3ihub.iitk.ac.in>
2025-01-30 16:46:14 +0530

    [update in]

--- src/lib/utils/icons.js
+++ src/lib/utils/icons.js
@@ -577,12 +577,12 @@ const GlobalICONS = {
         </svg>
     ),
     SIDEBAR_UNLOCK: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
             <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0-18 0" />
         </svg>
     ),
     SIDEBAR_LOCK: (
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20">
             <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
                 <path d="M11 12a1 1 0 1 0 2 0a1 1 0 1 0-2 0" />
                 <path d="M3 12a9 9 0 1 0 18 0a9 9 0 1 0-18 0" />
