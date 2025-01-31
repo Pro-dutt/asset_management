7e7dbc7e8af9fef9c9740e03d7b6008dbf6a5a52
Aausafalam <aausaf@c3ihub.iitk.ac.in>
2025-01-31 17:12:24 +0530

    [update in assets]

--- src/modules/endPoints/components/details/components/detailsBody/index.jsx
+++ src/modules/endPoints/components/details/components/detailsBody/index.jsx
@@ -1,28 +1,16 @@
 import React from "react";
 import styles from "./index.module.css";
+import DetailsUtils from "../../utils";
 
 const DetailsBody = ({ data }) => {
-    const formatText = (input) => {
-        return input
-            .replace(/wazuh/gi, "")
-            .replace(/([A-Z]+)([A-Z][a-z])/g, "$1 $2")
-            .replace(/([a-z\d])([A-Z])/g, "$1 $2")
-            .replace(/_/g, " ")
-            .replace(/^\s*/, "")
-            .replace(/\b\w/g, (char) => char.toUpperCase())
-            .replace(/\b(Ip|ip)\b/g, "IP");
-    };
-
     const renderValue = (value) => {
         if (typeof value === "object" && value !== null && "label" in value && "value" in value) {
-            // If the value is an object with "label" and "value" keys
             return (
                 <span>
-                    {formatText(value.label)}: {value.value || "N/A"}
+                    { DetailsUtils.formatText(value.label)}: {value.value || "N/A"}
                 </span>
             );
         } else if (typeof value === "string" || typeof value === "number") {
-            // If the value is a string or number
             return <span>{String(value).replace(/wazuh/gi, "")}</span>;
         }
         return null;
@@ -30,19 +18,17 @@ const DetailsBody = ({ data }) => {
 
     const renderListItems = () => {
         if (Array.isArray(data)) {
-            // If data is an array of objects with "label" and "value"
             return data.map((item, index) => (
                 <li key={index}>
                     <span>{item.label || "Unknown Label"}</span> <span>{item.value || "N/A"}</span>
                 </li>
             ));
         } else if (typeof data === "object" && data !== null) {
-            // If data is an object
             return Object.entries(data)
                 .map(([key, value]) =>
                     key !== "_id" ? (
                         <li key={key}>
-                            <span>{formatText(key)}</span> {renderValue(value)}
+                            <span>{DetailsUtils.formatText(key)}</span> {renderValue(value)}
                         </li>
                     ) : null
                 )
