6bd3453f5b94d33ad3f1e7079b66cb3c9be87233
Manoj Kumar <manojk@c3ihub.iitk.ac.in>
2025-02-03 15:48:44 +0530

    [update]

--- src/modules/desktops/components/stats/index.jsx
+++ src/modules/desktops/components/stats/index.jsx
@@ -3,15 +3,12 @@ import styles from "./styles/index.module.css";
 import useDesktopsStats from './hooks/useDesktopsStats';
 import StatCard from '@/components/Card/StatCard';
 import "./styles/index.css";
+import Stats from '@/components/stats';
 const DesktopsStats = () => {
     const {desktopsStatsConfig} = useDesktopsStats();
 
   return ( 
-    <div className={`stats_cards ${styles.container}`}>
-        {desktopsStatsConfig.map((item) => (
-                    <StatCard key={item._id} data={item} />
-                ))}
-    </div>
+    <Stats data={desktopsStatsConfig}/>
   )
 }
 
