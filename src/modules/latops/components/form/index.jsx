cc1c8641a175dd73be71238c51c4251419587b16
Aausafalam <aausaf@c3ihub.iitk.ac.in>
2025-02-03 17:46:24 +0530

    [update]

--- src/modules/latops/components/form/index.jsx
+++ src/modules/latops/components/form/index.jsx
@@ -1,15 +1,15 @@
 import DynamicForm from "@/components/form";
-import { useDesktopInfoForm } from "./hooks/useForm";
 import GlobalUtils from "@/lib/utils";
 import styles from "./styles/index.module.css";
+import { useDesktopInfoForm } from "@/modules/desktops/components/form/hooks/useForm";
 const LaptopInfoForm = ({ data, onCancel }) => {
     const { formConfig, handleFormSubmit, isLoading, DesktopInfoFormErrors } = useDesktopInfoForm(data);
 
     return (
         <div className={styles.container}>
             <DynamicForm
-                key="desktop-form"
-                formId="desktop"
+                key="laptop-form"
+                formId="laptop"
                 formData={formConfig}
                 formButtons={GlobalUtils.getFormButtons(isLoading, onCancel)}
                 onSubmit={handleFormSubmit}
