0f31842fc4f5f2ada0b0b2845cf512b8e4bbb0da
Aausafalam <aausaf@c3ihub.iitk.ac.in>
2025-02-04 17:48:53 +0530

    [update in assets]

--- src/modules/laptops/components/stats/index.jsx
+++ src/modules/laptops/components/stats/index.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./styles/index.module.css";
 import useLaptopsStats from "./hooks/useLaptopsStats";
 import "./styles/index.css";
 import StatCard from "@/components/Card/StatCard";
+import { useStats } from "@/services/context/stats";
 const LaptopsStats = () => {
-    const { laptopsStatsConfig } = useLaptopsStats();
-
+    const { statsCount } = useStats();
+    const { laptopsStatsConfig } = useLaptopsStats(statsCount.data);
+    useEffect(() => {
+        statsCount.execute({
+            params: { module: "laptops" },
+        });
+    }, []);
     return (
         <div className={styles.statsCardsContainer}>
             {laptopsStatsConfig.map((item, index) => (
