6f7245f679b82c03bdeb910361efcf0f910d21e2
Manoj Kumar <manojk@c3ihub.iitk.ac.in>
2025-02-04 17:20:11 +0530

    fdD

--- src/modules/dashboard/components/analytics/stats/utils/seeds.js
+++ src/modules/dashboard/components/analytics/stats/utils/seeds.js
@@ -2,7 +2,9 @@ const sampleDashboardStatsData = {
     totalLaptops: 567,
     totalDesktops: 100,
     totalNetworkDevices: 50,
-    totalVitualMachines: 50,
+    totalVitualMachines: 60,
+    totalServers: 30,
+    totalWebApplications: 9,
     // Add more data as needed...
-}
-export default sampleDashboardStatsData
+};
+export default sampleDashboardStatsData;
