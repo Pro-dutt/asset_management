0cd2cecbd5cc993628e6632725f4e5444fdad3ba
aryaanuj <anujarya6121998@gmail.com>
2025-02-06 11:59:26 +0530

    brand name added in sidebar

--- src/layout/components/Sidebar/data/sidebarIcon.js
+++ src/layout/components/Sidebar/data/sidebarIcon.js
@@ -226,9 +226,22 @@ const SidebarIcons = {
         </svg>
     ),
     LOGOUT: (
-        <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path opacity="0.5" d="M12 20C7.58172 20 4 16.4183 4 12C4 7.58172 7.58172 4 12 4" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
-            <path d="M10 12H20M20 12L17 9M20 12L17 15" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="icon icon-tabler icons-tabler-outline icon-tabler-logout"
+        >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2" />
+            <path d="M9 12h12l-3 -3" />
+            <path d="M18 15l3 -3" />
         </svg>
     ),
 };
